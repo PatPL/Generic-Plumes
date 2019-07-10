092c9c832480899e82c9c02c28ebed237490cd6e
PatPL <patrykwyp@poczta.fm>
2019-07-10 12:03:28 +0200

    init

---
+++ Unity/Assets/spreader.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+function Start () {
+	for (var i: int = 0; i < gameObject.transform.childCount; ++i) {
+		gameObject.transform.GetChild (i).localPosition = new Vector3 (i * 5, 0, 0);
+	}
+}
